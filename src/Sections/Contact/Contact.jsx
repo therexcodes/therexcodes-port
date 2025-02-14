759df130d58e609a5f8948a5685b736739e2f306
therexcodes <therexui@gmail.com>
2025-02-14 00:16:36 -1200

    commit email feature

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,10 +1,38 @@
 import styles from './Contact.module.css'
+import React, {useRef, useState} from 'react'
+import emailjs from 'emailjs-com'
 
 function Contact() {
+    const form = useRef();
+    const [messageSent, setMessageSent] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm(
+            "service_6d9beep",  // Replace with your EmailJS Service ID
+            "template_4pg3h0g", // Replace with your EmailJS Template ID
+            form.current,
+            "_xX3S3Q6SbJkcdjvd"  // Replace with your EmailJS Public Key
+        )
+        .then(
+            (result) => {
+            console.log("Email sent:", result.text);
+            setMessageSent(true);
+            },
+            (error) => {
+            console.error("Error sending email:", error.text);
+            }
+        );
+    };
+
+
     return (
         <section id='contact' className={styles.Cont}>
             <h1 className='sectionTitle'>CONTACT</h1>
-            <form action="">
+            {messageSent && <p className={styles.msgSent}>Message sent successfully!</p>}
+            <form ref={form} onSubmit={sendEmail}>
                 <div className='formGroup'>
                     <label htmlFor="name" hidden>
                         Name
@@ -16,15 +44,15 @@ function Contact() {
                     <label htmlFor="email" hidden>
                         Email
                     </label>
-                    <input type="name" id="name" placeholder='Email' required/>
+                    <input type="email" id="email" name='email' placeholder='Email' required/>
                     
                 </div>
 
                 <div className='formGroup'>
                     <label htmlFor="message" hidden>
-                        Name
+                        Message
                     </label>
-                    <textarea type="text" name='message' placeholder='Message' required>
+                    <textarea type="text"id='message' name='message' placeholder='Message' required>
                     </textarea>
                 </div>
                 <input type="submit" className='hover btn' value="Submit" />
